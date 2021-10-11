705d94fb767d2d438eaff7949607907fb4125fb6
Todor Pehlivanov <todor.pehlivanov@Todor-Pehlivanov.local>
2021-10-11 21:33:12 +0300

    Added index.php and config/config.php

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+// Here should be the database details
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Test Fight</title>
+<style>  
+.container{  
+text-align: center;  
+border: 7px solid red;  
+width: 300px;  
+height: 200px;  
+padding-top: 100px;  
+}  
+#btn{  
+font-size: 25px;  
+}  
+</style>  
+</head>
+<body>
+<div class="container">  
+<button id ="btn"> Attack </button>
+<button id ="btn"> Run </button>
+</div>  
+</body>
+</html>
