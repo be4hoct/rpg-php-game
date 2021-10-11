86e7980cbece6be35abd6792c0464caa121318c0
Todor Pehlivanov <todor.pehlivanov@Todor-Pehlivanov.local>
2021-10-11 21:52:37 +0300

    Modified lib/races.php, Added the Orc race

--- lib/races.php
+++ lib/races.php
@@ -1,13 +1,15 @@
 <?php
-// Let's define the Elf class;
-// Elves have high dexterity
+// Let's define the Elf class
+// Elves should have higher dex
 class Elf {
     // Let's define the starting stats
-    const start_vit = 11;
+    const start_vit = 10;
     const start_att = 9;
     const start_def = 10;
     const start_dex = 10;
 }
+// Let's define the Dwarf class
+// Dwarfes should have higher defence
 class Dwarf {
     // Let's define the starting stats
     const start_vit = 10;
@@ -15,12 +17,24 @@ class Dwarf {
     const start_def = 11;
     const start_dex = 9;
 }
+// Let's define the Human class
+// Humans should be a balanced class
 class Human {
     // Let's define the starting stats
     const start_vit = 9;
     const start_att = 9;
     const start_def = 9;
     const start_dex = 9;
+    const start_man = 9;
 }
-
+// Let's define the Orc class
+// Orcs should be bulky, higher HP and Att, low Mana
+class Orc {
+    // Let's define the starting stats
+    const start_vit = 12;
+    const start_att = 10;
+    const start_def = 8;
+    const start_dex = 8;
+    const start_man = 6;
+}
 ?>
