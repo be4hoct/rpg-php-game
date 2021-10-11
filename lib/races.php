4c9020859419e9589357d9823139c0de5412d77a
Todor Pehlivanov <todor.pehlivanov@Todor-Pehlivanov.local>
2021-10-11 21:38:22 +0300

    Added races.php

---
+++ lib/races.php
@@ -0,0 +1,26 @@
+<?php
+// Let's define the Elf class;
+// Elves have high dexterity
+class Elf {
+    // Let's define the starting stats
+    const start_vit = 11;
+    const start_att = 9;
+    const start_def = 10;
+    const start_dex = 10;
+}
+class Dwarf {
+    // Let's define the starting stats
+    const start_vit = 10;
+    const start_att = 8;
+    const start_def = 11;
+    const start_dex = 9;
+}
+class Human {
+    // Let's define the starting stats
+    const start_vit = 9;
+    const start_att = 9;
+    const start_def = 9;
+    const start_dex = 9;
+}
+
+?>
